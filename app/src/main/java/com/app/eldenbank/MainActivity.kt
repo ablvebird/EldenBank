b29186bafffe1af60f86be074904f709d23f3a6b
JCasRod <jcastillo.gothorum@gmail.com>
2024-01-25 18:20:03 +0100

    moar things

--- app/src/main/java/com/app/eldenbank/MainActivity.kt
+++ app/src/main/java/com/app/eldenbank/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.app.eldenbank
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    //User Data
     private var runeBalance: Int = 10000
+    private var userInput: Int = 0
+
+    //GUI Elements
+    private val inputBox:EditText = findViewById(R.id.runesEditText)
+    private val checkButton: Button = findViewById(R.id.checkButton)
+    private val depositButton: Button = findViewById(R.id.depositButton)
+    private val withdrawButton: Button = findViewById(R.id.withdrawButton)
+    private val exitButton: Button = findViewById(R.id.exitButton)
+    private val submitButton: Button = findViewById(R.id.submitButton)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setListeners()
-
     }
 
     private fun checkRunes() {
@@ -19,26 +30,36 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun depositRunes() {
-        runeBalance += 10
-        showToast("Deposited 10 Runes. Current Balance - $runeBalance")
+        /*  1) Make inputBox and submit button visible
+            2) Receive the user input and stores in variable
+            3) Adds the amount to the current runes
+         */
+        openInput()
+
+        runeBalance += userInput
+        showToast("Deposited $userInput Runes. Current Balance - $runeBalance")
     }
 
     private fun withdrawRunes() {
         if (runeBalance >= 10) {
             runeBalance -= 10
-            showToast("Withdrawn 10 Runes. Current Balance - $runeBalance")
+            showToast("Withdrawn $userInput Runes. Current Balance - $runeBalance")
         } else {
             showToast("Insufficient Runes for withdrawal.")
         }
     }
 
-    private fun setListeners(){
-        // Initialize UI components
-        val checkButton: Button = findViewById(R.id.checkButton)
-        val depositButton: Button = findViewById(R.id.depositButton)
-        val withdrawButton: Button = findViewById(R.id.withdrawButton)
-        val exitButton: Button = findViewById(R.id.exitButton)
+    private fun openInput(){
+        inputBox.visibility = View.VISIBLE
+        submitButton.visibility = View.VISIBLE
+        inputBox.requestFocus()
+    }
 
+    private fun handleInput(){
+
+    }
+
+    private fun setListeners(){
         checkButton.setOnClickListener {
             checkRunes()
         }
@@ -50,6 +71,9 @@ class MainActivity : AppCompatActivity() {
         }
         exitButton.setOnClickListener {
             exitApp()
+        }
+        submitButton.setOnClickListener{
+
         }
     }
 
